269285327e11e5b9fcfaa7879f91b66505756cd2
Syrgak <85344623+syrgak33@users.noreply.github.com>
2022-01-09 00:01:13 +0600

    Create rabbitmq_check.rb

---
+++ rabbitmq_check.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+
+describe package('rabbitmq-server'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+
+describe service('rabbitmq-server'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(15672) do
+  it { should be_listening }
+end
