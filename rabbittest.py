e935f35784c0702bced7028d7f861f9dac22024f
Syrgak <85344623+syrgak33@users.noreply.github.com>
2022-01-08 09:20:58 +0600

    Create rabbittest.py

---
+++ rabbittest.py
@@ -0,0 +1,11 @@
+def test_if_rabbitmq_is_installed(host):
+    rabbitmq_package = host.package('rabbitmq-server')
+    assert rabbitmq_package.is_installed
+
+def test_rabbitmq_runnnig_and_enabled(host):
+    rabbitmq = host.service("rabbitmq-server")
+    assert rabbitmq.is_running
+    assert rabbitmq.is_enabled
+def test_rabbitmqui_port(host):
+    rabuiport = host.socket('tcp://0.0.0.0:15672')
+    assert rabuiport.is_listening
