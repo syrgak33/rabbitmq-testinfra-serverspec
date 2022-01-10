6238380a24c4c22c3f974f64bdcf64fec1f7507a
Syrgak <85344623+syrgak33@users.noreply.github.com>
2022-01-10 20:12:21 +0600

    Update rabbittest.py

--- rabbittest.py
+++ rabbittest.py
@@ -6,6 +6,11 @@ def test_rabbitmq_runnnig_and_enabled(host):
     rabbitmq = host.service("rabbitmq-server")
     assert rabbitmq.is_running
     assert rabbitmq.is_enabled
+
 def test_rabbitmqui_port(host):
     rabuiport = host.socket('tcp://0.0.0.0:15672')
     assert rabuiport.is_listening
+    
+def test_output_rabbitmqctl(host):
+    output = host.check_output("sudo rabbitmqctl cluster_status")
+    assert 'rabbit@ubserver','rabbit@localhost' in output
