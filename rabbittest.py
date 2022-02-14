54f23ce3c3d53c753ee2e336dcfe2fc0c16d6f4b
Syrgak <85344623+syrgak33@users.noreply.github.com>
2022-02-14 07:11:13 +0600

    Update rabbittest.py

--- rabbittest.py
+++ rabbittest.py
@@ -14,3 +14,19 @@ def test_rabbitmqui_port(host):
 def test_output_rabbitmqctl(host):
     output = host.check_output("sudo rabbitmqctl cluster_status")
     assert 'rabbit@ubserver','rabbit@localhost' in output
+    
+def test_passwd_file(host):
+    passwd = host.file("/etc/passwd")
+    assert passwd.contains("root")
+    assert passwd.user == "root"
+    assert passwd.group == "root"
+
+def test_consul(host):
+    consuloutput = host.check_output("curl -G http://127.0.0.1:8500/v1/health/node/nt")
+    assert not "[]" in consuloutput
+
+def test_service_health(host):
+    serviceout = host.check_output("curl -G http://localhost:8500/v1/health/service/web?passing")
+    assert not "[]" in serviceout
+    
+
